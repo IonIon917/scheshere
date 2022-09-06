20878c4580f51172f496f4bd23a9d7770f5b434b
IonIon917 <pmmadjt@gmail.com>
2022-09-06 19:47:50 +0900

    Main headerの背景色の変更　検索ボタンの実装

--- lib/header.dart
+++ lib/header.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 import 'main_route.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const MyAppBar({Key? key}) : super(key: key);
+  MyAppBar({Key? key}) : super(key: key);
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
   @override
+  bool _searchBoolean = false;
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text('Main'),
-    );
+        backgroundColor: Color.fromARGB(255, 33, 243, 219),
+        title: Text('Main'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                setState(() {
+                  _searchBoolean = true;
+                });
+              })
+        ]);
   }
 }
