b86abf4247706acfabcb0caef4c59e901177a15c
IonIon917 <pmmadjt@gmail.com>
2022-09-20 11:58:47 +0900

    指定日時の色変更

--- lib/body.dart
+++ lib/body.dart
@@ -12,20 +12,31 @@ class MyBody extends StatefulWidget {
 class _MyBody extends State<MyBody> {
   @override
   DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
   CalendarFormat _calendarFormat = CalendarFormat.month;
   Widget build(BuildContext context) {
     return TableCalendar(
-      firstDay: DateTime.utc(2022, 1, 1),
-      lastDay: DateTime.utc(2050, 12, 31),
-      focusedDay: _focusedDay,
-      calendarFormat: _calendarFormat,
-      onFormatChanged: (format) {
-        if (_calendarFormat != format) {
-          setState(() {
-            _calendarFormat = format;
-          });
-        }
-      },
-    );
+        firstDay: DateTime.utc(2022, 1, 1),
+        lastDay: DateTime.utc(2050, 12, 31),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+          }
+        });
   }
 }
