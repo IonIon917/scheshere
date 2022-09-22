8dd5e2694bdb17bd5c719d0949a9209654cba660
IonIon917 <pmmadjt@gmail.com>
2022-09-22 17:52:14 +0900

    リスト表示の準備

--- lib/body.dart
+++ lib/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'main_route.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'dart:collection';
 
 class MyBody extends StatefulWidget {
   const MyBody({Key? key}) : super(key: key);
@@ -14,29 +15,85 @@ class _MyBody extends State<MyBody> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   CalendarFormat _calendarFormat = CalendarFormat.month;
+  Map<DateTime, List> _eventsList = {};
+
+  int getHashCode(DateTime key) {
+    return key.day * 1000000 + key.month * 10000 + key.year;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedDay = _focusedDay;
+    //サンプルのイベントリスト
+    _eventsList = {
+      DateTime.now().subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
+      DateTime.now(): ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
+      DateTime.now().add(Duration(days: 1)): [
+        'Event A8',
+        'Event B8',
+        'Event C8',
+        'Event D8'
+      ],
+      DateTime.now().add(Duration(days: 3)):
+          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
+      DateTime.now().add(Duration(days: 7)): [
+        'Event A10',
+        'Event B10',
+        'Event C10'
+      ],
+      DateTime.now().add(Duration(days: 11)): ['Event A11', 'Event B11'],
+      DateTime.now().add(Duration(days: 17)): [
+        'Event A12',
+        'Event B12',
+        'Event C12',
+        'Event D12'
+      ],
+      DateTime.now().add(Duration(days: 22)): ['Event A13', 'Event B13'],
+      DateTime.now().add(Duration(days: 26)): [
+        'Event A14',
+        'Event B14',
+        'Event C14'
+      ],
+    };
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-        firstDay: DateTime.utc(2022, 1, 1),
-        lastDay: DateTime.utc(2050, 12, 31),
-        focusedDay: _focusedDay,
-        calendarFormat: _calendarFormat,
-        onFormatChanged: (format) {
-          if (_calendarFormat != format) {
-            setState(() {
-              _calendarFormat = format;
-            });
-          }
-        },
-        selectedDayPredicate: (day) {
-          return isSameDay(_selectedDay, day);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          if (!isSameDay(_selectedDay, selectedDay)) {
-            setState(() {
-              _selectedDay = selectedDay;
-              _focusedDay = focusedDay;
-            });
-          }
-        });
+    final _events = LinkedHashMap<DateTime, List>(
+      equals: isSameDay,
+      hashCode: getHashCode,
+    )..addAll(_eventsList);
+
+    List getEventForDay(DateTime day) {
+      return _events[day] ?? [];
+    }
+
+    return Scaffold(
+        body: Column(children: [
+      TableCalendar(
+          firstDay: DateTime.utc(2022, 1, 1),
+          lastDay: DateTime.utc(2050, 12, 31),
+          focusedDay: _focusedDay,
+          calendarFormat: _calendarFormat,
+          onFormatChanged: (format) {
+            if (_calendarFormat != format) {
+              setState(() {
+                _calendarFormat = format;
+              });
+            }
+          },
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            if (!isSameDay(_selectedDay, selectedDay)) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay;
+              });
+            }
+          })
+    ]));
   }
 }
