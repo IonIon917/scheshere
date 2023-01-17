07a2fc36deb911bbe8cdfb05eda7897de7721a27
IonIon917 <pmmadjt@gmail.com>
2023-01-17 14:48:20 +0900

    Revert "DBとの接続"
    This reverts commit 46b8615cc72f5fdd8c1c1c2df5553ab6a463b5f7.

--- DB.dart
+++
@@ -1,7 +0,0 @@
-import 'package:mysql1/mysql1.dart';
-
-// Open a connection (scheshere should already exist)
-var settings = new ConnectionSettings(
-    host: 'localhost', port: 3306, user: 'root', db: 'scheshere');
-
-var conn = MySqlConnection.connect(settings);
