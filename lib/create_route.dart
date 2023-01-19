6288739a8a617d8f1621cc53aa3e0468f64ac944
IonIon917 <pmmadjt@gmail.com>
2023-01-19 15:07:15 +0900

    日付入力のファイル作成

--- lib/create_route.dart
+++ lib/create_route.dart
@@ -4,6 +4,7 @@ import 'body.dart';
 import 'schedule_route.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:intl/intl.dart';
+import 'hidukenyuuryoku_route.dart';
 
 class Create extends StatefulWidget {
   const Create({Key? key}) : super(key: key);
@@ -13,45 +14,13 @@ class Create extends StatefulWidget {
 }
 
 class _Create extends State<Create> {
-  dynamic dateTime;
-  dynamic dateFormat;
-
-  @override
-  void initState() {
-    super.initState();
-    dateTime = DateTime.now();
-    dateFormat = DateFormat("yyyy年MM月dd日").format(dateTime);
-  }
-
-  _datePicker(BuildContext context) async {
-    final DateTime? datePicked = await showDatePicker(
-        locale: const Locale("ja"),
-        context: context,
-        initialDate: dateTime,
-        firstDate: DateTime(2022),
-        lastDate: DateTime(2050));
-    if (datePicked != null && datePicked != dateTime) {
-      setState(() {
-        dateTime = datePicked;
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Center(
         child: Container(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(("$dateFormat")),
-                IconButton(
-                    onPressed: (() {
-                      _datePicker(context);
-                    }),
-                    icon: Icon(Icons.calendar_month_rounded))
-              ]),
+          child: Hiduke(),
         ),
       ),
     );
---
+++ lib/hidukenyuuryoku_route.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:scheshere/main.dart';
+import 'body.dart';
+import 'schedule_route.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
+
+class Hiduke extends StatefulWidget {
+  const Hiduke({Key? key}) : super(key: key);
+
+  @override
+  State<Hiduke> createState() => _Hiduke();
+}
+
+class _Hiduke extends State<Hiduke> {
+  dynamic dateTime;
+  dynamic dateFormat;
+
+  @override
+  void initState() {
+    super.initState();
+    dateTime = DateTime.now();
+    dateFormat = DateFormat("yyyy年MM月dd日").format(dateTime);
+  }
+
+  _datePicker(BuildContext context) async {
+    final DateTime? datePicked = await showDatePicker(
+        locale: const Locale("ja"),
+        context: context,
+        initialDate: dateTime,
+        firstDate: DateTime(2022),
+        lastDate: DateTime(2050));
+    if (datePicked != null && datePicked != dateTime) {
+      setState(() {
+        dateTime = datePicked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Container(
+            child: Column(children: <Widget>[
+              Text(("$dateFormat")),
+              IconButton(
+                  onPressed: (() {
+                    _datePicker(context);
+                  }),
+                  icon: Icon(Icons.calendar_month_rounded))
+            ]),
+          ),
+        ));
+  }
+}
