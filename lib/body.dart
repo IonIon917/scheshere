b8ba5557b5cf36ff0ac191a1ded7b9c0e47c6b0c
IonIon917 <pmmadjt@gmail.com>
2023-01-19 13:07:42 +0900

    Schedulu_createの削除2

--- lib/body.dart
+++ lib/body.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:scheshere/create_route.dart';
-import 'package:scheshere/schedule-create_route.dart';
 import 'main_route.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'dart:collection';
-import 'schedule-create_route.dart';
 import 'schedule_route.dart';
 
 class MyBody extends StatelessWidget {
