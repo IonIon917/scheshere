34cb42e5fa19676a3eb070f5024b872183c66603
IonIon917 <pmmadjt@gmail.com>
2023-01-20 16:06:05 +0900

    DropDownボタンの型

---
+++ lib/dropdown_route.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:scheshere/main.dart';
+import 'body.dart';
+import 'schedule_route.dart';
+import 'hidukenyuuryoku_route.dart';
+import 'package:riverpod/riverpod.dart';
+
+class DropDown extends StatefulWidget {
+  const DropDown({Key? key}) : super(key: key);
+
+  @override
+  State<DropDown> createState() => _DropDown();
+}
+
+class _DropDown extends State<DropDown> {
+  String? isSelectedItem = 'a';
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          DropdownButton(
+            items: [
+              DropdownMenuItem(
+                child: Text('a'),
+                value: 'a',
+              ),
+              DropdownMenuItem(
+                child: Text('b'),
+                value: 'b',
+              ),
+              DropdownMenuItem(
+                child: Text('c'),
+                value: 'c',
+              ),
+            ],
+            onChanged: (String? value) {
+              setState(() {
+                isSelectedItem = value;
+              });
+            },
+            value: isSelectedItem,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          Text('$isSelectedItem が選択されました。')
+        ],
+      )),
+    );
+  }
+}
