b4bf9dc77c820d35e79936b91547edb17dcd018a
IonIon917 <pmmadjt@gmail.com>
2023-01-23 22:07:21 +0900

    Revert "db"
    This reverts commit 677645ef25820ee43a0e8075ca1c9914e0579584.

---
+++ db/ScheduleModel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class Schedule {
+  final int dateTime;
+  final int id;
+  final String title;
+  final String place;
+  Category();
+}
